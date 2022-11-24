ec2ed57a56000f703d2a1364badfac309d9a6672
Viicos <65306057+Viicos@users.noreply.github.com>
2022-11-24 19:56:20 +0100

    doc: Fix module name

--- src/botframework/utils/module_loading.py
+++ src/botframework/utils/module_loading.py
@@ -2,7 +2,7 @@ from importlib import import_module
 
 
 def autodiscover_handlers(*module_names: str) -> None:
-    """Auto-discover commands module, to force an import and register handlers.
+    """Auto-discover handlers module, to force an import and register handlers.
 
     Args:
         module_names (str): Names of the modules to be imported.
